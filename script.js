4ecdd8275f5f5c0482c0bea821a6e3ccf416808c
Lori Culberson <bigskybandit@sbcglobal.net>
2022-06-23 20:45:16 -0400

    Add basic files

---
+++ script.js
@@ -0,0 +1,13 @@
+var body = document.querySelector('body')
+
+var header = document.createElement('h2')
+header.textContent = "I love a rainy night!"
+
+var divOne =  document.createElement('div')
+divOne.setAttribute('style', "background: green; font-size:2rem")
+
+var divTwo =  document.createElement('div')
+var sectionOne =  document.createElement('section')
+var sectionTwo =  document.createElement('section')
+
+body.appendChild(header)
