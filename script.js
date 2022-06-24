b22d1c75f7cf33e27ce464e842c406d7a4a662f2
Lori Culberson <bigskybandit@sbcglobal.net>
2022-06-23 21:22:40 -0400

    Add steps to js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// the only html tag on the page is the body tag
 var body = document.querySelector('body')
 
+//1. create an html element - h2
 var header = document.createElement('h2')
+
+//2. add text to the newly created element
 header.textContent = "I love a rainy night!"
 
-var divOne =  document.createElement('div')
-divOne.setAttribute('style', "background: green; font-size:2rem")
-
-var divTwo =  document.createElement('div')
-var sectionOne =  document.createElement('section')
-var sectionTwo =  document.createElement('section')
-
+// 3. append the header to the body tag
 body.appendChild(header)
+// var divOne =  document.createElement('div')
+// divOne.setAttribute('style', "background: green; font-size:2rem")
+
+// var divTwo =  document.createElement('div')
+// var sectionOne =  document.createElement('section')
+// var sectionTwo =  document.createElement('section')
